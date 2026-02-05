6becd4bbf3959a62a0809263bcf22dec64d215a7
Alan Jimenez <alanjq@outlook.com>
2026-02-04 23:54:43 -0500

    Proyecto React Native

---
+++ Semana-1/ejemplos/appRNative/Home.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function HomeScreen() {
+    return (
+        <View>
+            <Text>Hola desde la pantalla de inicio</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
