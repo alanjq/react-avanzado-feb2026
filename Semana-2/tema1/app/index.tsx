3a71ce5e008277af7c50a9e06c6e61c6dc0ec8d5
Alan Jimenez <alanjq@outlook.com>
2026-02-09 22:19:20 -0500

    Proyecto reiniciado en blanco

---
+++ Semana-2/tema1/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edita el archivo app/index.tsx para ver los cambios en esta pantalla.</Text>
+    </View>
+  );
+}
